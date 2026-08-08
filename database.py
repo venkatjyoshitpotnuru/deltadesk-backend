db1f1f7bc307e00f9db9c530af747990b6965216
jyoshit.ai_enthusiast <venkatjyoshitpotnuru7@gmail.com>
2026-08-08 17:30:48 +0530

    Modified database and added dotenv file

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:jyoshitP7@localhost:5432/deltadesk"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
